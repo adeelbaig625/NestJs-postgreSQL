83519b34ea794f196967d326d5f6c2cff3ce2ae3
adeelbaig625 <adeelbaig625@gmail.com>
2023-01-17 02:41:36 +0500

    auth service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from 'database/data.source';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import RedisModule from './redis/redis.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions)],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions), RedisModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/auth.service.ts
+++ src/user/auth.service.ts
@@ -1,3 +1,12 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import RedisService from 'src/redis/redis.service';
+import { Repository } from 'typeorm';
+import { UserModel } from './user.entity';
+import { v4 as uuid } from 'uuid';
+import { appEnv } from '../../helpers/env.helper';
+import { config } from 'dotenv';
+
+config();
 export class AuthModel {
   Id: number;
   // User: User | null;
@@ -7,5 +16,33 @@ export class AuthModel {
     if (user) {
       this.User = user;
     }
+  }
+}
+export default class AuthService {
+  constructor(
+    @InjectRepository(UserModel)
+    private userRepository: Repository<UserModel>,
+    private _cacheService: RedisService,
+  ) {}
+
+  private _generateToken() {
+    return uuid();
+  }
+
+  async CreateSession(userId: number): Promise<string> {
+    const Token = this._generateToken();
+    const Auth = new AuthModel(userId);
+    await this._cacheService.Set(Token, Auth, appEnv('TOKEN_EXPIRATION', 3600));
+    return Token;
+  }
+
+  async GetSession(token: string): Promise<AuthModel> {
+    const Auth: AuthModel = await this._cacheService.Get(token);
+    if (!Auth) return null;
+    Auth.User = await this.userRepository.findOneBy({
+      Id: Auth.Id,
+      DeletedAt: null,
+    });
+    return Auth;
   }
 }
